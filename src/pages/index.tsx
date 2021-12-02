430654aca87a2da7be3d4e56f40735860b6eae2b
Roosevelt innocent <howdy.roosevelt@gmail.com>
2021-12-02 12:08:02 +0100

    update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,103 @@
+import { Button } from '@chakra-ui/button'
+import { Box, Container, Flex, Heading, Text } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import WebsiteLayout from '../components/layouts/website'
 
 const Home: NextPage = () => {
   return (
     <WebsiteLayout title="Home">
-      <h2>Connecting every bussiness to a customer</h2>
+      <header className="header">
+        <Box
+          bgImage="url('/hedr-bg-home.svg')"
+          bgPosition="center center"
+          bgRepeat="no-repeat"
+          bgSize="80% 120%"
+          mt="12"
+        >
+          <Container maxW="container.lg" py="20" centerContent>
+            <Box maxW="48rem" mb="8">
+              <Heading textAlign="center" size="lg" fontSize="64" mb="4">
+                Connecting every bussiness to a customer
+              </Heading>
+              <Text fontSize="2xl" textAlign="center">
+                On the world’s largest P2P ecommerce stores
+              </Text>
+            </Box>
+
+            <Flex p="4">
+              <Box>
+                <Button variant="outline" mr="4">
+                  Go Shopping
+                </Button>
+              </Box>
+              <Box>
+                <Button>Setup my store</Button>
+              </Box>
+            </Flex>
+          </Container>
+        </Box>
+      </header>
+
+      <Container maxW="container.lg" pt="20" centerContent>
+        <Box maxW="48rem">
+          <Text fontSize="xl" textAlign="center" mb="2">
+            <strong>Qua</strong> is bringing every bussiness online
+          </Text>
+          <Heading textAlign="center" size="md" fontSize="36">
+            Sell to your customers in 3 easy steps...
+          </Heading>
+        </Box>
+      </Container>
+
+      <Container maxW="container.xl">
+        <Box
+          bgImage="url('/images/setup-store.jpg')"
+          bgPosition="center center"
+          bgRepeat="no-repeat"
+          bgSize="contain"
+          MaxW="100%"
+          height="100vh"
+        />
+      </Container>
+      <Container centerContent>
+        <Flex p="4" mt="-16">
+          <Button variant="outline" size="lg" width="348px">
+            Setup my store
+          </Button>
+        </Flex>
+      </Container>
+
+      <Container maxW="container.lg" pt="20" centerContent>
+        <Box maxW="48rem">
+          <Heading textAlign="center" size="md" fontSize="36" mb="4">
+            Customers shop in a breath with ease.
+          </Heading>
+          <Text fontSize="xl" textAlign="center">
+            Direct shopping from your favorite dealer is breez·y
+          </Text>
+        </Box>
+      </Container>
+      <Container maxW="container.xl">
+        <Box
+          bgImage="url('/images/product-view.jpg')"
+          bgPosition="center center"
+          bgRepeat="no-repeat"
+          bgSize="contain"
+          MaxW="100%"
+          height="100vh"
+        />
+      </Container>
+
+      <Container maxW="container.lg">
+        <Box
+          bgImage="url('/images/footer-cta.svg')"
+          bgPosition="center center"
+          bgRepeat="no-repeat"
+          bgSize="contain"
+          MaxW="100%"
+          height="100vh"
+        />
+      </Container>
     </WebsiteLayout>
   )
 }
