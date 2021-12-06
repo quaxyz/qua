5eb07c96fd2e064e3d4c0985b5e4f78f3f9c09a3
Marvin Kome <marvinkome@gmail.com>
2021-12-06 14:18:55 +0100

    add mobile layout

--- src/components/layouts/store-dashboard.tsx
+++ src/components/layouts/store-dashboard.tsx
@@ -27,10 +27,18 @@ const navLinks = [
 const DashboardLayout: React.FC = ({ children }) => {
   const router = useRouter();
   return (
-    <Grid templateColumns="240px 1fr" minH="100vh">
-      <chakra.aside bg="#000000" px={4} py={8}>
+    <Grid
+      templateColumns="240px 1fr"
+      templateRows="70px 1fr 70px"
+      templateAreas={{
+        base: `"topbar topbar" "main main" "bottombar bottombar"`,
+        md: `"sidebar main" "sidebar main" "sidebar main"`,
+      }}
+      minH="100vh"
+    >
+      <chakra.aside gridArea="sidebar" bg="#000000" px={4} py={8} display={{ base: "none", md: "block" }}>
         <Stack spacing={8} minH="100%">
-          <Heading fontSize="2xl" color="#fff" px={3}>
+          <Heading fontWeight="800" fontSize="2xl" color="#fff" px={3}>
             Frowth
           </Heading>
 
@@ -70,7 +78,53 @@ const DashboardLayout: React.FC = ({ children }) => {
         </Stack>
       </chakra.aside>
 
-      <chakra.main>{children}</chakra.main>
+      <chakra.header gridArea="topbar" bg="#000000" px={5} display={{ base: "block", md: "none" }}>
+        <Stack direction="row" alignItems="center" justify="space-between" h="100%" w="100%">
+          <Heading fontWeight="800" fontSize="xl" color="#fff">
+            Frowth
+          </Heading>
+
+          <Wallet
+            ButtonProps={{
+              variant: "outline",
+              color: "#FFF",
+              rounded: "8px",
+              size: "sm",
+              borderColor: "rgb(255 255 255 / 16%)",
+              rightIcon: <Icon as={CgMore} />,
+              _hover: { bg: "transparent", borderColor: "rgb(255 255 255 / 48%)" },
+            }}
+          />
+        </Stack>
+      </chakra.header>
+
+      <chakra.main gridArea="main">{children}</chakra.main>
+
+      <chakra.aside gridArea="bottombar" bg="#000000" display={{ base: "block", md: "none" }}>
+        <Stack direction="row" alignItems="center" justify="space-around" h="100%" w="100%">
+          {navLinks.map((NavLink, idx) => (
+            <NextLink key={idx} href={NavLink.url} passHref>
+              <Link
+                borderBottom="none"
+                color="#FFF"
+                {...(router.asPath.includes(NavLink.url) ? { textDecoration: "underline" } : {})}
+              >
+                <Stack spacing={2} align="center">
+                  <Icon
+                    boxSize={5}
+                    as={(props) => (
+                      <NavLink.icon {...(router.asPath.includes(NavLink.url) ? { set: "bold" } : {})} {...props} />
+                    )}
+                  />
+                  <Text fontWeight="normal" color="inherit" fontSize="xs" as="span">
+                    {NavLink.name}
+                  </Text>
+                </Stack>
+              </Link>
+            </NextLink>
+          ))}
+        </Stack>
+      </chakra.aside>
     </Grid>
   );
 };
