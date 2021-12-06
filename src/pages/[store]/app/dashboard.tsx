eae6bd05157300991e7d036080bce1c6962997d8
Marvin Kome <marvinkome@gmail.com>
2021-12-06 14:21:04 +0100

    small page update

--- src/pages/[store]/app/dashboard.tsx
+++ src/pages/[store]/app/dashboard.tsx
@@ -1,9 +1,15 @@
+import React from "react";
+import Head from "next/head";
 import type { NextPage } from "next";
 import StoreDashboardLayout from "components/layouts/store-dashboard";
 
 const Dashboard: NextPage = () => {
   return (
     <StoreDashboardLayout>
+      <Head>
+        <title>Dashboard - Frowth</title>
+      </Head>
+
       <h2>interesting stuff </h2>
     </StoreDashboardLayout>
   );
--- src/pages/[store]/app/products.tsx
+++ src/pages/[store]/app/products.tsx
@@ -1,8 +1,13 @@
+import React from "react";
+import Head from "next/head";
 import StoreDashboardLayout from "components/layouts/store-dashboard";
 
 const Products = () => {
   return (
     <StoreDashboardLayout>
+      <Head>
+        <title>Products - Frowth</title>
+      </Head>
       <h1>Products page</h1>
     </StoreDashboardLayout>
   );
