9b57020989cb8f2d37ce347fe290199ab053d6cf
Marvin Kome <marvinkome@gmail.com>
2022-01-24 15:45:59 +0100

    fix dashboard orders

--- src/pages/[store]/app/dashboard.tsx
+++ src/pages/[store]/app/dashboard.tsx
@@ -1,3 +1,8 @@
+import React, { FunctionComponent, useState } from "react";
+import { GetServerSideProps } from "next";
+import Link from "components/link";
+import prisma from "libs/prisma";
+import StoreDashboardLayout from "components/layouts/store-dashboard";
 import {
   Box,
   Button,
@@ -10,22 +15,10 @@ import {
   PopoverArrow,
   PopoverContent,
   PopoverTrigger,
-  Spacer,
   Stack,
+  StackDivider,
   Text,
 } from "@chakra-ui/react";
-import StoreDashboardLayout from "components/layouts/store-dashboard";
-import Link from "components/link";
-import { OrderPaymentStatus, OrderStatus } from "components/order-pill";
-import Api from "libs/api";
-import prisma from "libs/prisma";
-import { truncateAddress } from "libs/utils";
-import type { NextPage } from "next";
-import { GetServerSideProps } from "next";
-import { useRouter } from "next/router";
-import React, { FunctionComponent, useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import {
   ArrowDown,
   ArrowUp,
@@ -35,7 +28,11 @@ import {
   Plus,
 } from "react-iconly";
 import { HiOutlineClock } from "react-icons/hi";
-import { useInfiniteQuery } from "react-query";
+import { OrderPaymentStatus, OrderStatus } from "components/order-pill";
+import { truncateAddress } from "libs/utils";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 interface IInfoCard {
   bgColor?: string;
@@ -128,260 +125,7 @@ const InfoCard = (props: IInfoCard) => {
   );
 };
 
-const statusColor: any = {
-  cancelled: {
-    bgColor: "red.200",
-    color: "red.800",
-  },
-  unfulfilled: {
-    bgColor: "rgba(254, 238, 205, 1)",
-    color: "rgba(120, 81, 2, 1)",
-  },
-  fulfilled: {
-    bgColor: "rgba(205, 254, 240, 1)",
-    color: "rgba(2, 120, 87, 1)",
-  },
-};
-
-const paymentStatusColor: any = {
-  paid: {
-    bgColor: "rgba(205, 254, 240, 1)",
-    color: "rgba(2, 120, 87, 1)",
-  },
-  contact_seller: {
-    bgColor: "rgba(254, 238, 205, 1)",
-    color: "rgba(120, 81, 2, 1)",
-  },
-  unpaid: {
-    bgColor: "rgba(254, 238, 205, 1)",
-    color: "rgba(120, 81, 2, 1)",
-  },
-};
-
-const OrderGrid = ({ initialData }: any) => {
-  const router = useRouter();
-
-  const queryResp = useInfiniteQuery({
-    queryKey: "store-dashboard-orders",
-    initialData: { pages: [initialData], pageParams: [] },
-    getNextPageParam: (lastPage: any) => {
-      if (lastPage?.length >= 0) {
-        return lastPage[lastPage?.length - 1]?.id;
-      }
-    },
-    queryFn: async ({ pageParam = 0 }) => {
-      const { payload }: any = await Api().get(
-        `/app/orders?cursor=${pageParam}`
-      );
-      return payload;
-    },
-  });
-  const isEmpty = !queryResp?.data?.pages.filter((p) => p.length).length;
-
-  return (
-    <Box>
-      {!isEmpty ? (
-        <>
-          {/* <Box maxW="403px" pt="5px" pb={{ base: "28px", md: "45px" }}>
-            <InputGroup>
-              <InputLeftElement fontSize="1rem" pointerEvents="none">
-                <Icon
-                  boxSize="10px"
-                  as={() => <Search set="light" primaryColor="#0E0F0F" />}
-                />
-              </InputLeftElement>
-              <Input
-                type="text"
-                color="rgba(180, 182, 184, 1)"
-                placeholder="Search"
-              />
-            </InputGroup>
-          </Box> */}
-
-          <Box pb="3rem">
-            <Stack
-              direction="row"
-              display={{ base: "none", md: "flex" }}
-              my="8px"
-              px="1.5rem"
-              py="1rem"
-            >
-              <Flex w="100%">
-                <Text>Order ID</Text>
-              </Flex>
-              <Flex w="100%">
-                <Text>Customer</Text>
-              </Flex>
-              <Flex w="100%">
-                <Text>Status</Text>
-              </Flex>
-              <Flex w="100%">
-                <Text>Payment</Text>
-              </Flex>
-            </Stack>
-
-            {queryResp.data?.pages?.map((page) =>
-              page.map((data: any) => (
-                <React.Fragment key={data.id}>
-                  <Stack
-                    mb="1rem"
-                    direction={{ base: "column", md: "row" }}
-                    border="0.5px solid rgba(0, 0, 0, 0.12)"
-                    p="1.4rem"
-                    alignItems="center"
-                  >
-                    <Flex w="100%" justify="space-between">
-                      <Text display={{ base: "inline-block", md: "none" }}>
-                        Order ID
-                      </Text>
-
-                      <Link
-                        href={`/app/orders/${data.id}`}
-                        fontSize="16px"
-                        fontWeight={{ base: "400", md: "600" }}
-                      >
-                        #{data.id}
-                      </Link>
-                    </Flex>
-                    <Flex w="100%" justify="space-between">
-                      <Text display={{ base: "inline-block", md: "none" }}>
-                        Customer
-                      </Text>
-                      <Text
-                        fontSize="14px"
-                        textAlign="right"
-                        fontWeight={{ base: "600", md: "600" }}
-                        mb={{ base: "2", md: "0" }}
-                      >
-                        {data.customerDetails?.name}{" "}
-                        {truncateAddress(data.customerAddress, 4)}
-                      </Text>
-                    </Flex>
-                    <Flex w="100%" justify="space-between">
-                      <Text display={{ base: "inline-block", md: "none" }}>
-                        Status
-                      </Text>
-                      <OrderStatus
-                        fontSize="14px"
-                        fontWeight="500"
-                        lineHeight="1.5"
-                        borderRadius="8px"
-                        px="12px"
-                        py="4px"
-                        textAlign="center"
-                        status={data.status}
-                      />
-                    </Flex>
-                    <Flex w="100%" justify="space-between">
-                      <Text display={{ base: "inline-block", md: "none" }}>
-                        Payment
-                      </Text>
-                      <OrderPaymentStatus
-                        fontSize="14px"
-                        fontWeight="500"
-                        lineHeight="1.5"
-                        borderRadius="8px"
-                        px="12px"
-                        py="4px"
-                        textAlign="center"
-                        status={data.paymentStatus}
-                      />
-                    </Flex>
-                  </Stack>
-                </React.Fragment>
-              ))
-            )}
-
-            {queryResp.hasNextPage && (
-              <Stack align="center">
-                <Button
-                  onClick={() => queryResp.fetchNextPage()}
-                  isLoading={queryResp.isFetchingNextPage}
-                  variant="solid-outline"
-                >
-                  Load more
-                </Button>
-              </Stack>
-            )}
-          </Box>
-        </>
-      ) : (
-        <Container maxW="100%" py={8} px={{ base: "4", md: "12" }}>
-          <Stack
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            mt={{ base: "4rem", md: "6rem" }}
-          >
-            <Image
-              src="/svg/Bag.svg"
-              alt="Add Icon"
-              layout="fixed"
-              w={{ base: "20", md: "100" }}
-              h={{ base: "20", md: "100" }}
-              mb="1"
-            />
-            <Stack alignItems="center" textAlign="center" justify="center">
-              <Text fontSize="xl" fontWeight="bold" color="#000">
-                No recent orders
-              </Text>
-              <Text fontSize="lg" pb="14px">
-                Add a product to your store to recieve orders
-              </Text>
-
-              <Link
-                as={Button}
-                href={`/${router?.query.store}/app/products/new`}
-                variant="primary"
-              >
-                <Plus
-                  set="bold"
-                  primaryColor="#ffffff"
-                  style={{ marginRight: "14px" }}
-                />
-                New Product
-              </Link>
-            </Stack>
-          </Stack>
-        </Container>
-      )}
-    </Box>
-  );
-};
-
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const store = (params?.store as string) || "";
-
-  const data = await prisma.order.findMany({
-    take: 10,
-    where: {
-      Store: {
-        name: store,
-      },
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-    select: {
-      id: true,
-      customerAddress: true,
-      customerDetails: true,
-      status: true,
-      paymentStatus: true,
-    },
-  });
-
-  return {
-    props: {
-      initialData: data,
-      layoutProps: {
-        title: "Dashboard",
-      },
-    },
-  };
-};
-
-const Dashboard: NextPage = () => {
+const Page = ({ orders }: any) => {
   const cards = [
     {
       bgColor: "#FEEECD",
@@ -415,19 +159,16 @@ const Dashboard: NextPage = () => {
     },
   ];
 
-  // const isOrderList = true;
   const [startDate, setStartDate] = useState(new Date());
   const handleChange = (e: any) => {
     setStartDate(e);
   };
 
-  return (
-    <StoreDashboardLayout>
-      {/* <Head>
-        <title>Dashboard - Frowth</title>
-      </Head> */}
+  const ordersIsEmpty = !orders.length;
 
-      <Box px={{ base: "1rem", md: "4rem" }}>
+  return (
+    <Container maxW="100%" py={8} px={{ base: "4", md: "12" }}>
+      <Stack spacing={10} divider={<StackDivider />}>
         <Stack pos="relative">
           <Stack
             pos="absolute"
@@ -449,6 +190,7 @@ const Dashboard: NextPage = () => {
               </Text>
             </Stack>
           </Stack>
+
           <Stack opacity="0.2" pointerEvents="none" userSelect="none">
             <Flex justifyContent="space-between" py="2rem" alignItems="center">
               <Heading
@@ -501,7 +243,6 @@ const Dashboard: NextPage = () => {
             <Box>
               <Stack
                 width="100%"
-                // minW="max-content"
                 justifyContent="space-between"
                 direction={{ base: "column", md: "row" }}
                 spacing={{ base: "1rem", md: "24px" }}
@@ -516,31 +257,188 @@ const Dashboard: NextPage = () => {
           </Stack>
         </Stack>
 
-        <Spacer py="6" />
-        <hr />
-
-        <Flex
-          justifyContent="space-between"
-          mt={{ base: "1.5rem", md: "2rem" }}
-          mb={{ base: "1rem", md: "2rem" }}
-          pt="1rem"
-          alignItems="center"
-        >
-          <Heading
-            as="h2"
-            fontSize={{ base: "18px", md: "24px" }}
-            fontWeight="500"
-            color="#000"
+        <Stack>
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
           >
-            Activity
-          </Heading>
+            <Heading
+              as="h2"
+              fontSize={{ base: "18px", md: "24px" }}
+              fontWeight="500"
+              color="#000"
+            >
+              Activity
+            </Heading>
 
-          <Link href={`/app/orders`}>View all orders</Link>
-        </Flex>
-        <OrderGrid />
-      </Box>
-    </StoreDashboardLayout>
+            <Link href={`/app/orders`}>View all orders</Link>
+          </Stack>
+
+          {!ordersIsEmpty ? (
+            <Box>
+              <Stack
+                direction="row"
+                display={{ base: "none", md: "flex" }}
+                my="8px"
+                px="1rem"
+                py="1rem"
+              >
+                <Flex w="100%">
+                  <Text>Order ID</Text>
+                </Flex>
+                <Flex w="100%">
+                  <Text>Customer</Text>
+                </Flex>
+                <Flex w="100%">
+                  <Text>Status</Text>
+                </Flex>
+                <Flex w="100%">
+                  <Text>Payment</Text>
+                </Flex>
+              </Stack>
+
+              {orders.map((order: any) => (
+                <React.Fragment key={order.id}>
+                  <Stack
+                    mb="1rem"
+                    direction={{ base: "column", md: "row" }}
+                    border="0.5px solid rgba(0, 0, 0, 0.12)"
+                    p="1.4rem"
+                    alignItems="center"
+                  >
+                    <Flex w="100%" justify="space-between">
+                      <Text display={{ base: "inline-block", md: "none" }}>
+                        Order ID
+                      </Text>
+
+                      <Link
+                        href={`/app/orders/${order.id}`}
+                        fontSize="16px"
+                        fontWeight={{ base: "400", md: "600" }}
+                      >
+                        #{order.id}
+                      </Link>
+                    </Flex>
+                    <Flex w="100%" justify="space-between">
+                      <Text display={{ base: "inline-block", md: "none" }}>
+                        Customer
+                      </Text>
+                      <Text
+                        fontSize="14px"
+                        textAlign="right"
+                        fontWeight={{ base: "600", md: "600" }}
+                        mb={{ base: "2", md: "0" }}
+                      >
+                        {order.customerDetails?.name}{" "}
+                        {truncateAddress(order.customerAddress, 4)}
+                      </Text>
+                    </Flex>
+                    <Flex w="100%" justify="space-between">
+                      <Text display={{ base: "inline-block", md: "none" }}>
+                        Status
+                      </Text>
+                      <OrderStatus
+                        fontSize="14px"
+                        fontWeight="500"
+                        lineHeight="1.5"
+                        borderRadius="8px"
+                        px="12px"
+                        py="4px"
+                        textAlign="center"
+                        status={order.status}
+                      />
+                    </Flex>
+                    <Flex w="100%" justify="space-between">
+                      <Text display={{ base: "inline-block", md: "none" }}>
+                        Payment
+                      </Text>
+                      <OrderPaymentStatus
+                        fontSize="14px"
+                        fontWeight="500"
+                        lineHeight="1.5"
+                        borderRadius="8px"
+                        px="12px"
+                        py="4px"
+                        textAlign="center"
+                        status={order.paymentStatus}
+                      />
+                    </Flex>
+                  </Stack>
+                </React.Fragment>
+              ))}
+            </Box>
+          ) : (
+            <Stack
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              py={{ base: "4rem", md: "6rem" }}
+            >
+              <Image
+                src="/svg/Bag.svg"
+                alt="Add Icon"
+                layout="fixed"
+                w={{ base: "20", md: "100" }}
+                h={{ base: "20", md: "100" }}
+                mb="1"
+              />
+              <Stack alignItems="center" textAlign="center" justify="center">
+                <Text fontSize="xl" fontWeight="bold" color="#000">
+                  No recent orders
+                </Text>
+                <Text fontSize="lg" pb="14px">
+                  Add a product to your store to recieve orders
+                </Text>
+
+                <Link as={Button} href={`/app/products/new`} variant="primary">
+                  <Plus
+                    set="bold"
+                    primaryColor="#ffffff"
+                    style={{ marginRight: "14px" }}
+                  />
+                  New Product
+                </Link>
+              </Stack>
+            </Stack>
+          )}
+        </Stack>
+      </Stack>
+    </Container>
   );
 };
 
-export default Dashboard;
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const store = (params?.store as string) || "";
+
+  const data = await prisma.order.findMany({
+    take: 5,
+    where: {
+      Store: {
+        name: store,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      id: true,
+      customerAddress: true,
+      customerDetails: true,
+      status: true,
+      paymentStatus: true,
+    },
+  });
+
+  return {
+    props: {
+      orders: data,
+      layoutProps: {
+        title: "Dashboard",
+      },
+    },
+  };
+};
+
+Page.Layout = StoreDashboardLayout;
+export default Page;
